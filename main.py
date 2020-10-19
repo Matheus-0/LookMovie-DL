06e78d32d9789737ac3f5c8fffd2dfc6955698e4
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2020-10-19 07:55:02 -0300

    Add inquirer.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Initial code.')
+import inquirer
+
+questions = [
+    inquirer.List(
+        'type',
+        'Do you want to look for movies or TV shows?',
+        ['Movies', 'TV shows'],
+        carousel=True
+    ),
+    inquirer.Text(
+        'query',
+        'Search'
+    )
+]
+
+answer = inquirer.prompt(questions)
